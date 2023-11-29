6b00791d290a6f88a74d4432e64d8a1f97b4fbdd
Soapery <darian.a.ronald@gmail.com>
2023-11-29 12:47:44 -0600

    Fixed database code.

--- db_operations.py
+++ db_operations.py
@@ -7,65 +7,55 @@ class DBOperations:
     def __init__(self, db_name='weather_data.db'):
         """ Initializes an instance of the DBOperations class. """
         self.db_name = db_name
-        self.conn = None
 
     def initialize_db(self):
         """ Initializes a sqlite database file with the given DB name. """
-        try:
-            self.conn = sqlite3.connect(self.db_name)
-            print("Opened database successfully.")
-        except Exception as e:
-            print("Error opening DB:", e)
-
-        try:
-            cursor = self.conn.cursor()
-            cursor.execute('''create table if not exists weather
-                            (id integer primary key autoincrement not null,
-                            sample_date text,
-                            location text,
-                            min_temp real,
-                            max_temp real,
-                            avg_temp real,
-                            unique (sample_date, location));''')
-            self.conn.commit()
-        except Exception as e:
-            print("Error creating table:", e)
+        with DBCM(self.db_name) as cursor:
+            try:
+                cursor.execute('''create table if not exists weather
+                                (id integer primary key autoincrement not null,
+                                sample_date text,
+                                location text,
+                                min_temp real,
+                                max_temp real,
+                                avg_temp real,
+                                unique (sample_date, location));''')
+            except Exception as e:
+                print("Error creating table:", e)
 
     def purge_data(self):
         """ Purges the database of all entries. """
-        try:
-            cursor = self.conn.cursor()
-            cursor.execute('''delete from weather''')
-            self.conn.commit()
-        except Exception as e:
-            print("Error purging database:", e)
+        with DBCM(self.db_name) as cursor:
+            try:
+                cursor.execute('''delete from weather''')
+            except Exception as e:
+                print("Error purging database:", e)
 
     def save_data(self, weather_data, location="Winnipeg, MB"):
         """ Accepts a dictionary of weather data and inserts the given values into the database. """
-        try:
-            cursor = self.conn.cursor()
-            for date, data in weather_data.items():
-                max_temp = data.get('Max')
-                min_temp = data.get('Min')
-                avg_temp = data.get('Mean')
-                cursor.execute('''
-                    INSERT OR IGNORE INTO weather (sample_date, location, min_temp, max_temp, avg_temp)
-                    VALUES (?, ?, ?, ?)
-                ''', (date, location, min_temp, max_temp, avg_temp))
-            self.conn.commit()
-        except Exception as e:
-            print("Error inserting data:", e)
+        with DBCM(self.db_name) as cursor:
+            try:
+                for date, data in weather_data.items():
+                    max_temp = data.get('Max')
+                    min_temp = data.get('Min')
+                    avg_temp = data.get('Mean')
+                    cursor.execute('''
+                        INSERT OR IGNORE INTO weather (sample_date, location, min_temp, max_temp, avg_temp)
+                        VALUES (?, ?, ?, ?, ?)
+                    ''', (date, location, min_temp, max_temp, avg_temp))
+            except Exception as e:
+                print("Error inserting data:", e)
 
     def fetch_data(self):
         """ Returns all rows from the database. """
-        try:
-            cursor = self.conn.cursor()
-            cursor.execute("select * from weather")
-            rows = cursor.fetchall()
-            return rows
-        except Exception as e:
-            print("Error fetching from weather:", e)
-            return e
+        with DBCM(self.db_name) as cursor:
+            try:
+                cursor.execute("select * from weather")
+                rows = cursor.fetchall()
+                return rows
+            except Exception as e:
+                print("Error fetching from weather:", e)
+                return e
 
 if __name__ == "__main__":
     weather_scraper = WeatherScraper()
@@ -73,9 +63,6 @@ if __name__ == "__main__":
 
     weather_data = weather_scraper.scrape_weather_data()
 
-    with DBCM():
-        db_ops.initialize_db()
-        db_ops.purge_data()
-        db_ops.save_data(weather_data)
-        fetched_data = db_ops.fetch_data()
-        print(fetched_data)
+    db_ops.initialize_db()
+    db_ops.purge_data()
+    db_ops.save_data(weather_data, "Winnipeg, MB")
--- dbcm.py
+++ dbcm.py
@@ -1,20 +1,28 @@
 import sqlite3
 
 class DBCM:
-    """ Represents a context manager for interacting with a SQLite database. """
+    """ Context Manager class for handling SQLite3 database connections. """
     def __init__(self, db_name='weather_data.db'):
         """ Initializes an instance of the DBCM class. """
         self.db_name = db_name
+        self.conn = None
 
     def __enter__(self):
-        """ Establishes a database connection and returns a cursor. """
-        self.conn = sqlite3.connect(self.db_name)
-        return self.conn.cursor()
+        """ Establishes a connection with the SQLite3 database and returns a cursor. """
+        try:
+            self.conn = sqlite3.connect(self.db_name)
+            print("Opened database successfully.")
+            return self.conn.cursor()
+        except Exception as e:
+            print("Error opening DB: ", e)
+            return e
 
-    def __exit__(self, exc_type, exc_value, traceback):
-        """ Commits and closes database connections, or rolls back changes if an exception occurs. """
-        if exc_type or exc_value or traceback:
+    def __exit__(self, exc_type, exc_val, exc_trace):
+        """ Closes connection upon completing database tasks. """
+        if exc_type or exc_val or exc_trace:
             self.conn.rollback()
+            print("Error occurred. Rollback executed.")
         else:
             self.conn.commit()
+            print("Changes committed.")
         self.conn.close()

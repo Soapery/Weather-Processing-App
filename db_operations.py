c78172dfa273b5d212c938b65007c6d863bfa2f5
Soapery <darian.a.ronald@gmail.com>
2023-12-04 19:57:53 -0600

    Added logging.

--- db_operations.py
+++ db_operations.py
@@ -2,12 +2,16 @@ import sqlite3
 from dbcm import DBCM
 from scrape_weather import WeatherScraper
 from plot_operations import PlotOperations
+from weather_logger import WeatherLogger
 
 class DBOperations:
     """ Represents a database with functions to initialize, insert data, read data, and purge the database. """
     def __init__(self, db_name='weather_data.sqlite'):
         """ Initializes an instance of the DBOperations class. """
         self.db_name = db_name
+
+        logger = WeatherLogger()
+        self.logger = logger.get_logger()
 
     def initialize_db(self):
         """ Initializes a sqlite database file with the given DB name. """
@@ -21,16 +25,19 @@ class DBOperations:
                                 max_temp real,
                                 avg_temp real,
                                 unique (sample_date, location));''')
+
+                self.logger.info("Database initialized successfully.")
             except Exception as e:
-                print("Error creating table:", e)
+                self.logger.critical("Database initialization failed! Error:", e)
 
     def purge_data(self):
         """ Purges the database of all entries. """
         with DBCM(self.db_name) as cursor:
             try:
                 cursor.execute('''delete from weather''')
+                self.logger.info("Database purged successfully.")
             except Exception as e:
-                print("Error purging database:", e)
+                self.logger.error("Database purge failed! Error:", e)
 
     def save_data(self, weather_data, location="Winnipeg, MB"):
         """ Accepts a dictionary of weather data and inserts the given values into the database. """
@@ -44,8 +51,9 @@ class DBOperations:
                         INSERT OR IGNORE INTO weather (sample_date, location, min_temp, max_temp, avg_temp)
                         VALUES (?, ?, ?, ?, ?)
                     ''', (date, location, min_temp, max_temp, avg_temp))
+                self.logger.info(f"Database insert of {len(weather_data)} items completed successfully.")
             except Exception as e:
-                print("Error inserting data:", e)
+                self.logger.critical("Database insert failed! Error:", e)
 
     def fetch_data(self):
         """ Returns all rows from the database. """
@@ -56,7 +64,7 @@ class DBOperations:
                 cursor.execute("select * from weather")
                 rows = cursor.fetchall()
 
-                for row in rows: 
+                for row in rows:
                     date = row[1]
                     min_temp = row[3]
                     max_temp = row[4]
@@ -64,9 +72,10 @@ class DBOperations:
 
                     weather_data[date] = {'Min': min_temp, 'Max': max_temp, 'Mean': avg_temp}
 
+                self.logger.info(f"Database rows from database \"{self.db_name}\" retrieved successfully.")
                 return weather_data
             except Exception as e:
-                print("Error fetching from weather:", e)
+                print(f"Error fetching rows from \"{self.db_name}\". Error:", e)
                 return e
 
 if __name__ == "__main__":
@@ -83,7 +92,7 @@ if __name__ == "__main__":
     plot_data = db_ops.fetch_data()
 
     boxplot_start_year = int(input("Enter a starting year for your boxplot: "))
-    boxplot_end_year = int(input("Enter an end year for your boxplot: "))    
+    boxplot_end_year = int(input("Enter an end year for your boxplot: "))
     plotter.create_boxplot(plot_data, boxplot_start_year, boxplot_end_year)
 
     lineplot_year = int(input("Enter a year for your lineplot: "))

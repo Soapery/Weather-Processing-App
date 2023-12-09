d51d81603684bd5b4fe166dc7101b39dc77688b5
Soapery <darian.a.ronald@gmail.com>
2023-12-09 16:14:51 -0600

    Fixed linting errors.

--- weather_logger.py
+++ weather_logger.py
@@ -1,34 +1,35 @@
 """
-This module contains the WeatherLogger class, which returns a formatted logger for logging data in this application.
+This module contains the WeatherLogger class,
+which returns a formatted logger for logging data in this application.
 """
 
 import logging
 
 class WeatherLogger:
-  """ Represents a formatted logging object. """
-  def __init__(self):
-    """ Initializes an instance of the WeatherLogger class. """
-    self.logger = logging.getLogger('log')
-    self.logger.setLevel(logging.DEBUG)
+    """ Represents a formatted logging object. """
+    def __init__(self):
+        """ Initializes an instance of the WeatherLogger class. """
+        self.logger = logging.getLogger('log')
+        self.logger.setLevel(logging.DEBUG)
 
-    # File handler for low-level logs
-    file_handler = logging.FileHandler('weather_processor.log')
-    file_handler.setLevel(logging.DEBUG)
+        # File handler for low-level logs
+        file_handler = logging.FileHandler('weather_processor.log')
+        file_handler.setLevel(logging.DEBUG)
 
-    # Console handler is given higher log level
-    console_handler = logging.StreamHandler()
-    console_handler.setLevel(logging.ERROR)
+        # Console handler is given higher log level
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.ERROR)
 
-    # Adding formatting
-    formatter = logging.Formatter(
-        '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    console_handler.setFormatter(formatter)
-    file_handler.setFormatter(formatter)
+        # Adding formatting
+        formatter = logging.Formatter(
+            '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        console_handler.setFormatter(formatter)
+        file_handler.setFormatter(formatter)
 
-    # add the handlers to logger
-    self.logger.addHandler(console_handler)
-    self.logger.addHandler(file_handler)
+        # add the handlers to logger
+        self.logger.addHandler(console_handler)
+        self.logger.addHandler(file_handler)
 
-  def get_logger(self):
-    """ Returns the formatted logger. """
-    return self.logger
+    def get_logger(self):
+        """ Returns the formatted logger. """
+        return self.logger

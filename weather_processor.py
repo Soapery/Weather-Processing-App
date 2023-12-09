3d36fd2d80a05241545a7bf8f52c10908800a5e2
Soapery <darian.a.ronald@gmail.com>
2023-12-09 15:48:49 -0600

    Added module docstring.

---
+++ weather_processor.py
@@ -0,0 +1,9 @@
+"""
+This module contains the WeatherProcessor class, which shows the user a menu for interfacing with the functions of this application.
+WeatherProcessor contains functions for starting a scraping of weather data, prompting a database update, and generating box and line plots.
+"""
+
+class WeatherProcessor():
+  def __init__(self):
+    print("hi")
+
